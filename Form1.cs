9e9c1c7cd8c69953f899d78b5b8877fdf6dd51f3
민정 <mjchae2776@naver.com>
2026-03-12 16:40:45 +0900

    버튼이 도망갈 때와 버튼을 잡았을 때 소리 출력

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using System.Media;
 
 namespace CatchButton
 {
@@ -13,7 +14,17 @@ namespace CatchButton
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //잡았을 때 '띠링' 하는 느낌의 소리
+            SystemSounds.Asterisk.Play();
 
+            // 1. 소리 재생
+            SystemSounds.Asterisk.Play();
+
+            // 2. 메시지 박스 출력
+            MessageBox.Show("축하합니다~!");
+
+            // 3. 게임 재시작 준비 (제목 표시줄 초기화)
+            this.Text = "Catch the button version 1.2";
         }
 
         private void btnTarget_MouseEnter(object sender, EventArgs e)
@@ -31,6 +42,9 @@ namespace CatchButton
             btnTarget.Location = new Point(nextX, nextY);
             
             this.Text = $"버튼위치: ({nextX}, {nextY})";
+
+            //도망갈 때 '띵' 하는 가벼운 경고음
+            SystemSounds.Beep.Play();
         }
     }
 }
