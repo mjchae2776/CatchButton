cd359414dcca09f7f3e611c894e5abf0bc27f983
민정 <mjchae2776@naver.com>
2026-03-12 16:43:29 +0900

    버튼이 도망갈 때와 버튼을 잡았을 때 소리 출력 및 버튼을 잡았을 때 메세지 출력

--- Form1.cs
+++ Form1.cs
@@ -14,16 +14,16 @@ namespace CatchButton
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //잡았을 때 '띠링' 하는 느낌의 소리
+            
             SystemSounds.Asterisk.Play();
 
-            // 1. 소리 재생
+            
             SystemSounds.Asterisk.Play();
 
-            // 2. 메시지 박스 출력
+            
             MessageBox.Show("축하합니다~!");
 
-            // 3. 게임 재시작 준비 (제목 표시줄 초기화)
+            
             this.Text = "Catch the button version 1.2";
         }
 
