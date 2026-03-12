4842d9b06776b942677a260a91516b44ff5d7aa2
민정 <mjchae2776@naver.com>
2026-03-12 17:36:17 +0900

    버튼을 20번 놓치면 "Game Over" 메시지를 출력함, ＇다시시작＇ 버튼을 누르면 게임 관련된 모든 정보를 초기화하고 게임을 다시 시작하게 함

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,12 @@
         {
             btnTarget = new Button();
             colorDialog1 = new ColorDialog();
+            btnReset = new Button();
             SuspendLayout();
             // 
             // btnTarget
             // 
+            btnTarget.AutoSize = true;
             btnTarget.BackColor = SystemColors.ActiveCaption;
             btnTarget.Font = new Font("함초롬돋움 확장", 48F, FontStyle.Bold, GraphicsUnit.Point, 129);
             btnTarget.Location = new Point(96, 124);
@@ -46,20 +48,36 @@
             btnTarget.Click += button1_Click;
             btnTarget.MouseEnter += btnTarget_MouseEnter;
             // 
+            // btnReset
+            // 
+            btnReset.BackColor = Color.Gainsboro;
+            btnReset.Font = new Font("함초롬바탕 확장", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnReset.Location = new Point(294, 177);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(248, 86);
+            btnReset.TabIndex = 1;
+            btnReset.Text = "다시 시작";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Visible = false;
+            btnReset.Click += btnReset_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnReset);
             Controls.Add(btnTarget);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnTarget;
         private ColorDialog colorDialog1;
+        private Button btnReset;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,10 +6,28 @@ namespace CatchButton
     public partial class Form1 : Form
     {
         private int score = 0;
+        private int missCount = 0;
         public Form1()
         {
             InitializeComponent();
+            ResetGame();
             this.Text = "Catch the button version 1.2";
+        }
+
+        private void ResetGame()
+        {
+            score = 0;
+            missCount = 0;
+
+            btnTarget.Visible = true;
+
+            btnTarget.Enabled = true; 
+            btnTarget.Size = new Size(400,150);
+            btnTarget.Text = "나를 잡아봐!";
+            btnTarget.Font = new Font(btnTarget.Font.FontFamily, 48f); 
+
+            btnReset.Visible = false; 
+            this.Text = "Catch the button v1.2";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -40,34 +58,36 @@ namespace CatchButton
 
         private void btnTarget_MouseEnter(object sender, EventArgs e)
         {
-            
-
-            SystemSounds.Beep.Play();
-
-            Random rd = new Random();
-
-            int maxX = this.ClientSize.Width - btnTarget.Width;
-            int maxY = this.ClientSize.Height - btnTarget.Height;
-
-
-            int nextX = rd.Next(0, maxX);
-            int nextY = rd.Next(0, maxY);
-            
-            btnTarget.Location = new Point(nextX, nextY);
-            
-            this.Text = $"버튼위치: ({nextX}, {nextY})";
-
-            //도망갈 때 '띵' 하는 가벼운 경고음
-            SystemSounds.Beep.Play();
-
+            missCount++;
             score -= 10;
 
             
+            if (missCount >= 20)
+            {
+                GameOver();
+                return; 
+            }
+
+            Random rd = new Random();
+            int maxX = this.ClientSize.Width - btnTarget.Width;
+            int maxY = this.ClientSize.Height - btnTarget.Height;
+            int nextX = rd.Next(0, Math.Max(1, maxX));
+            int nextY = rd.Next(0, Math.Max(1, maxY));
 
             btnTarget.Location = new Point(nextX, nextY);
+            this.Text = $"점수: {score} | 놓친 횟수: {missCount}/20";
+            SystemSounds.Beep.Play();
+        }
+        private void GameOver()
+        {
+            MessageBox.Show("Game Over");
+            btnTarget.Visible = false;
+            btnReset.Visible = true;   
+        }
 
-
-            this.Text = $"점수: {score} | 버튼위치: ({nextX}, {nextY})";
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetGame(); 
         }
     }
 }
